7d78248ff1d0008cf88d7b54c84b6c405b5dfab7
Мишуняев Никита Сергеевич <36814225+Mishunyayev-Nikita@users.noreply.github.com>
2019-06-03 13:40:10 +0700

    add course materials

---
+++ assignments/assignment2/model.py
@@ -0,0 +1,72 @@
+import numpy as np
+
+from layers import FullyConnectedLayer, ReLULayer, softmax_with_cross_entropy, l2_regularization
+
+
+class TwoLayerNet:
+    """ Neural network with two fully connected layers """
+
+    def __init__(self, n_input, n_output, hidden_layer_size, reg):
+        """
+        Initializes the neural network
+
+        Arguments:
+        n_input, int - dimension of the model input
+        n_output, int - number of classes to predict
+        hidden_layer_size, int - number of neurons in the hidden layer
+        reg, float - L2 regularization strength
+        """
+        self.reg = reg
+        # TODO Create necessary layers
+        raise Exception("Not implemented!")
+
+    def compute_loss_and_gradients(self, X, y):
+        """
+        Computes total loss and updates parameter gradients
+        on a batch of training examples
+
+        Arguments:
+        X, np array (batch_size, input_features) - input data
+        y, np array of int (batch_size) - classes
+        """
+        # Before running forward and backward pass through the model,
+        # clear parameter gradients aggregated from the previous pass
+        # TODO Set parameter gradient to zeros
+        # Hint: using self.params() might be useful!
+        raise Exception("Not implemented!")
+        
+        # TODO Compute loss and fill param gradients
+        # by running forward and backward passes through the model
+        
+        # After that, implement l2 regularization on all params
+        # Hint: self.params() is useful again!
+        raise Exception("Not implemented!")
+
+        return loss
+
+    def predict(self, X):
+        """
+        Produces classifier predictions on the set
+
+        Arguments:
+          X, np array (test_samples, num_features)
+
+        Returns:
+          y_pred, np.array of int (test_samples)
+        """
+        # TODO: Implement predict
+        # Hint: some of the code of the compute_loss_and_gradients
+        # can be reused
+        pred = np.zeros(X.shape[0], np.int)
+
+        raise Exception("Not implemented!")
+        return pred
+
+    def params(self):
+        result = {}
+
+        # TODO Implement aggregating all of the params
+
+        raise Exception("Not implemented!")
+
+        return result
---
+++ assignments/assignment3/model.py
@@ -0,0 +1,60 @@
+import numpy as np
+
+from layers import (
+    FullyConnectedLayer, ReLULayer,
+    ConvolutionalLayer, MaxPoolingLayer, Flattener,
+    softmax_with_cross_entropy, l2_regularization
+    )
+
+
+class ConvNet:
+    """
+    Implements a very simple conv net
+
+    Input -> Conv[3x3] -> Relu -> Maxpool[4x4] ->
+    Conv[3x3] -> Relu -> MaxPool[4x4] ->
+    Flatten -> FC -> Softmax
+    """
+    def __init__(self, input_shape, n_output_classes, conv1_channels, conv2_channels):
+        """
+        Initializes the neural network
+
+        Arguments:
+        input_shape, tuple of 3 ints - image_width, image_height, n_channels
+                                         Will be equal to (32, 32, 3)
+        n_output_classes, int - number of classes to predict
+        conv1_channels, int - number of filters in the 1st conv layer
+        conv2_channels, int - number of filters in the 2nd conv layer
+        """
+        # TODO Create necessary layers
+        raise Exception("Not implemented!")
+
+    def compute_loss_and_gradients(self, X, y):
+        """
+        Computes total loss and updates parameter gradients
+        on a batch of training examples
+
+        Arguments:
+        X, np array (batch_size, height, width, input_features) - input data
+        y, np array of int (batch_size) - classes
+        """
+        # Before running forward and backward pass through the model,
+        # clear parameter gradients aggregated from the previous pass
+
+        # TODO Compute loss and fill param gradients
+        # Don't worry about implementing L2 regularization, we will not
+        # need it in this assignment
+        raise Exception("Not implemented!")
+
+    def predict(self, X):
+        # You can probably copy the code from previous assignment
+        raise Exception("Not implemented!")
+
+    def params(self):
+        result = {}
+
+        # TODO: Aggregate all the params from all the layers
+        # which have parameters
+        raise Exception("Not implemented!")
+
+        return result
